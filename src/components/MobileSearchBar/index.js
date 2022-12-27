8f04263ba45aae134dd8bd35de5cf2efa0824577
vikas <visparmar119@gmail.com>
2022-12-27 16:29:51 +0530

    mobile Search bar updation

--- src/components/MobileSearchBar/index.js
+++ src/components/MobileSearchBar/index.js
@@ -1,18 +1,35 @@
 import React from 'react'
 import './style.css'
 import SearchIcon from '@mui/icons-material/Search';
+import TuneOutlinedIcon from '@mui/icons-material/TuneOutlined';
 
 
-export default function MobileSearchBar(){
+export default function MobileSearchBar() {
 
-    return(
-        <div className='mobile-search-bar'>
-            <SearchIcon sx={{display:"flex"}}/>
-            <div className='where-to'>Where to?
-                <p className='Anywhere'>Anywhere . Any week . Add guests</p>
-            </div>
+    return (
+
+        <div className='mobile-search-bar' >
             
-            
-            </div>
+            <div className='search-icon'> <SearchIcon /></div>
+           
+
+                <div className='Anywhere'>
+                    <div className='where-to'>Where to?</div>
+                    <span className='Anywhere-name'>Anywhere 
+                    <span className='Anywhere-dot'>.</span>  
+                    <span className='Anywhere-name'>Any Week</span>   
+                    <span className='Anywhere-dot'>.</span>
+                    <span className='Anywhere-name'>Add guests</span></span>
+                    
+                   
+                    
+                    
+                </div>
+                <div className='filter'><TuneOutlinedIcon/></div>
+
+        </div>
+
+
+
     )
 }
